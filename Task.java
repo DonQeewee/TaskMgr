551cbaf99c64923dfa5a16eb5189ef1ae539cf81
Ayomide <primedaug@outlook.com>
2023-11-07 23:12:37 +0100

    taskmngr assignment

--- Task.java
+++ Task.java
@@ -1,3 +1,37 @@
 public class Task {
 
+    private int id;
+    private String title;
+    private String description;
+    private  boolean isCompleted;
+
+
+
+    public Task(int id, String title, String description, boolean isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.isCompleted = false;  //task completion status is pending by default.
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
 }
